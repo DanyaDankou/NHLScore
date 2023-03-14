177d6a72f30d36d77f181ba1f637aaef4766224a
Danya Dankou <93947618+DanyaDankou@users.noreply.github.com>
2023-03-14 20:48:16 +0300

    Revert "teamsTable first commit"

---
+++ NHLScore/NHLScore/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NHLScore
+//
+//  Created by comp on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
